8963003f2d4ba52dc99cdca939604c005993c583
Daniel C <daniel@daniel.com>
2020-01-31 10:42:35 -0600

    Finished SQL Queries Parts 1 and 2.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,22 @@
 ## Part 1: Test it with SQL
-Describe job;
+
+SHOW COLUMNS FROM techjobs.job;
+
+--   Results:
+--   NAME     TYPE          NULLABLE      KEYS
+--   id       int           Not Nullable  PRIMARY KEY
+--   employer varchar(255)
+--   name     varchar(255)
+--   skills   varchar(255)
 
 ## Part 2: Test it with SQL
+
+SELECT * FROM techjobs.employer
+WHERE location = "St. Louis";
+
+--   Results
+--   id     name      location
+--   27     Boeing    St. Louis
 
 ## Part 3: Test it with SQL
 
